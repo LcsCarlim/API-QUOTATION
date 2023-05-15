804a190780c28d6fe336faf0c70361af634dd1bd
Lucas <lucascarlim0520@gmail.com>
2023-05-15 17:15:53 -0300

    fix: swagger css

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 require('dotenv').config();
 const config = require('.');
+const express = require('express');
+const mime = require('mime');
 
 try {
   config.open(process.env.MONGODB_URI).then(() => {
-    config.app.listen(3333, () => {
+    const app = express();
+
+    // Definir tipo de conteúdo MIME para arquivos estáticos
+    app.use((req, res, next) => {
+      const mimeType = mime.getType(req.url);
+      if (mimeType) {
+        res.setHeader('Content-Type', mimeType);
+      }
+      next();
+    });
+
+    app.listen(3333, () => {
       console.log('Server is running!');
     });
   });
 } catch (error) {
   console.log(error.message);
-};
+}
