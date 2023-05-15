b9b68f95526f9abb6ddbb7b61621a23bfc39fe9f
Lucas <lucascarlim0520@gmail.com>
2023-05-15 14:55:59 -0300

    fix: swagger

--- src/index.js
+++ src/index.js
@@ -15,6 +15,9 @@ app.use(express.json());
 app.use('/docs', swaggerUi.serve);
 app.get(swaggerUi.setup(swaggerDocs));
 
+routes.use('/api-docs', swaggerUi.serve);
+routes.get('/api-docs', swaggerUi.setup(swaggerDocs));
+
 app.use(routes);
 
 module.exports = { app, open };
