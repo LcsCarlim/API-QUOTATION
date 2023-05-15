62009beccad6089dbedb5b035dbb5bf96a19058a
Lucas <lucascarlim0520@gmail.com>
2023-05-15 17:19:38 -0300

    not work

--- src/server.js
+++ src/server.js
@@ -1,34 +1,12 @@
 require('dotenv').config();
 const config = require('.');
-const express = require('express');
-const path = require('path');
-const mime = require('mime');
 
 try {
   config.open(process.env.MONGODB_URI).then(() => {
-    const app = express();
-
-    // Definir tipo de conteúdo MIME para arquivos estáticos
-    app.use((req, res, next) => {
-      const mimeType = mime.getType(req.url);
-      if (mimeType) {
-        res.setHeader('Content-Type', mimeType);
-      }
-      next();
-    });
-
-    // Configurar o diretório de arquivos estáticos
-    app.use(express.static(path.join(__dirname, 'public')));
-
-    // Roteamento
-    app.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    });
-
-    app.listen(3333, () => {
+    config.app.listen(3333, () => {
       console.log('Server is running!');
     });
   });
 } catch (error) {
   console.log(error.message);
-}
+};
