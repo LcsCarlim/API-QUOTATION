54a7103baebca3b15476ccb52da1f3926a2f7c31
Lucas <lucascarlim0520@gmail.com>
2023-05-15 17:31:10 -0300

    fix: internal error

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const routes = require('./routes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./swagger.json');
 const mongoose = require('mongoose');
-const mime = require('mime-types');
 
 async function open (uri) {
   await mongoose.connect(uri);
@@ -15,15 +14,6 @@ app.use(express.json());
 
 routes.use('/docs', swaggerUi.serve);
 routes.get('/docs', swaggerUi.setup(swaggerDocs));
-
-app.use((req, res, next) => {
-  const filePath = req.url.split('?')[0]; // Remove a query string da URL, se houver
-  const mimeType = mime.contentType(filePath);
-  if (mimeType) {
-    res.set('Content-Type', mimeType);
-  }
-  next();
-});
 
 app.use(routes);
 
