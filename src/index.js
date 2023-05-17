7574632701edd146e0499e56e18fdda160494274
Lucas <lucascarlim0520@gmail.com>
2023-05-17 10:38:11 -0300

    feat: cors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const routes = require('./routes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./swagger.json');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 async function open (uri) {
   await mongoose.connect(uri);
@@ -14,6 +15,8 @@ const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger
 
 app.use(express.json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, { customCssUrl: CSS_URL }));
+
+app.use(cors);
 
 app.use(routes);
 
