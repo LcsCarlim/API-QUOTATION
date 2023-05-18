4608de1020e75e1b80ce0270b118500c74814005
Lucas <lucascarlim0520@gmail.com>
2023-05-18 11:43:46 -0300

    fix: img

--- src/services/Quotation/GetQuotationService.js
+++ src/services/Quotation/GetQuotationService.js
@@ -49,7 +49,7 @@ module.exports = class GetQuotationService {
       pctChange: Number(json.EURBRL.pctChange),
       create_date: json.EURBRL.create_date,
       account_id: user_id,
-      image: 'https://ibb.co/8NBGxgW'
+      image: 'https://ibb.co/8NBGxgW' // a
     };
 
     const BTC = {
