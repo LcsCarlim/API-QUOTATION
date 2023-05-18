e52cf7d8f3bc75dcd101a9638f886d557018b9b8
Lucas <lucascarlim0520@gmail.com>
2023-05-18 11:52:18 -0300

    fix: img

--- src/services/Quotation/GetQuotationService.js
+++ src/services/Quotation/GetQuotationService.js
@@ -49,7 +49,7 @@ module.exports = class GetQuotationService {
       pctChange: Number(json.EURBRL.pctChange),
       create_date: json.EURBRL.create_date,
       account_id: user_id,
-      image: 'https://www.flickr.com/photos/198343813@N07/52906488642/in/dateposted-public/'
+      image: 'https://flic.kr/p/2oBazQE'
     };
 
     const BTC = {
