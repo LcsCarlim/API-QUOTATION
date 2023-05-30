0130b8e18acf7ec8f57160a7fe6226b169315676
Lucas <lucascarlim0520@gmail.com>
2023-05-30 10:10:50 -0300

    fix: format to number

--- src/services/Quotation/GetQuotationService.js
+++ src/services/Quotation/GetQuotationService.js
@@ -1,5 +1,5 @@
 const getCurrencyGateway = require('../../gateway/GetAPIDataGateway');
-const toBRL = require('../../helpers/formatBRL');
+// const toBRL = require('../../helpers/formatBRL');
 const QuotationModel = require('../../database/model/QuotationModel');
 
 module.exports = class GetQuotationService {
@@ -13,9 +13,9 @@ module.exports = class GetQuotationService {
     const USD = {
       code: json.USDBRL.code,
       name: 'Dólar-Americano',
-      bid: toBRL(json.USDBRL.bid),
-      high: toBRL(json.USDBRL.high),
-      low: toBRL(json.USDBRL.low),
+      bid: Number(json.USDBRL.bid),
+      high: Number(json.USDBRL.high),
+      low: Number(json.USDBRL.low),
       pctChange: Number(json.USDBRL.pctChange),
       create_date: json.USDBRL.create_date,
       // account_id: user_id,
@@ -25,9 +25,9 @@ module.exports = class GetQuotationService {
     const CAD = {
       code: json.CADBRL.code,
       name: 'Dólar-Canadense',
-      bid: toBRL(json.CADBRL.bid),
-      high: toBRL(json.CADBRL.high),
-      low: toBRL(json.CADBRL.low),
+      bid: Number(json.CADBRL.bid),
+      high: Number(json.CADBRL.high),
+      low: Number(json.CADBRL.low),
       pctChange: Number(json.CADBRL.pctChange),
       create_date: json.CADBRL.create_date,
       // account_id: user_id,
@@ -37,9 +37,9 @@ module.exports = class GetQuotationService {
     const EUR = {
       code: json.EURBRL.code,
       name: 'Euro',
-      bid: toBRL(json.EURBRL.bid),
-      high: toBRL(json.EURBRL.high),
-      low: toBRL(json.EURBRL.low),
+      bid: Number(json.EURBRL.bid),
+      high: Number(json.EURBRL.high),
+      low: Number(json.EURBRL.low),
       pctChange: Number(json.EURBRL.pctChange),
       create_date: json.EURBRL.create_date,
       // account_id: user_id,
@@ -49,9 +49,9 @@ module.exports = class GetQuotationService {
     const BTC = {
       code: json.BTCBRL.code,
       name: 'Bitcoin',
-      bid: toBRL(json.BTCBRL.bid),
-      high: toBRL(json.BTCBRL.high),
-      low: toBRL(json.BTCBRL.low),
+      bid: Number(json.BTCBRL.bid),
+      high: Number(json.BTCBRL.high),
+      low: Number(json.BTCBRL.low),
       pctChange: Number(json.BTCBRL.pctChange),
       create_date: json.BTCBRL.create_date,
       // account_id: user_id,
@@ -61,9 +61,9 @@ module.exports = class GetQuotationService {
     const ETH = {
       code: json.ETHBRL.code,
       name: 'Ethereum',
-      bid: toBRL(json.ETHBRL.bid),
-      high: toBRL(json.ETHBRL.high),
-      low: toBRL(json.ETHBRL.low),
+      bid: Number(json.ETHBRL.bid),
+      high: Number(json.ETHBRL.high),
+      low: Number(json.ETHBRL.low),
       pctChange: Number(json.ETHBRL.pctChange),
       create_date: json.ETHBRL.create_date,
       // account_id: user_id,
@@ -73,9 +73,9 @@ module.exports = class GetQuotationService {
     const LTC = {
       code: json.LTCBRL.code,
       name: 'Litecoin',
-      bid: toBRL(json.LTCBRL.bid),
-      high: toBRL(json.LTCBRL.high),
-      low: toBRL(json.LTCBRL.low),
+      bid: Number(json.LTCBRL.bid),
+      high: Number(json.LTCBRL.high),
+      low: Number(json.LTCBRL.low),
       pctChange: Number(json.LTCBRL.pctChange),
       create_date: json.LTCBRL.create_date,
       // account_id: user_id,
@@ -85,9 +85,9 @@ module.exports = class GetQuotationService {
     const DOGE = {
       code: json.DOGEBRL.code,
       name: 'Dogecoin',
-      bid: toBRL(json.DOGEBRL.bid),
-      high: toBRL(json.DOGEBRL.high),
-      low: toBRL(json.DOGEBRL.low),
+      bid: Number(json.DOGEBRL.bid),
+      high: Number(json.DOGEBRL.high),
+      low: Number(json.DOGEBRL.low),
       pctChange: Number(json.DOGEBRL.pctChange),
       create_date: json.DOGEBRL.create_date,
       // account_id: user_id,
